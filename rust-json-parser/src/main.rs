f29247c9548c15ee96ceca325cff6bfe3fbbe589
Ben Griffith <bengriffith@outlook.com>
2026-04-03 21:44:50 -0600

    update output scenarios

--- rust-json-parser/src/main.rs
+++ rust-json-parser/src/main.rs
@@ -1,11 +1,17 @@
 use rust_json_parser::JsonParser;
 
 fn main() {
-    let input_string = "1 2 3 false".to_string();
-    let input: Vec<&str> = input_string.split_whitespace().collect();
+    let input_string = "1 2 3 false @".to_string();
+    let parser = JsonParser::new(&input_string);
+    match parser {
+        Ok(_) => println!("parser created without error"),
+        Err(e) => println!("error: {:?}", e),
+    }
 
-    let mut parser = JsonParser::new(&input_string).unwrap();
+    let input_string2 = "4 5 6 true".to_string();
+    let input: Vec<&str> = input_string2.split_whitespace().collect();
+    let mut parser2 = JsonParser::new(&input_string2).unwrap();
     for _ in input {
-        println!("parsed value: {:?}", parser.parse());
+        println!("parsed value: {:?}", parser2.parse());
     }
 }
