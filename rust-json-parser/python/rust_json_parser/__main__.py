8e4e595b1f68cc28129d1042de7d7b161abefbf6
Ben Griffith <bengriffith@outlook.com>
2026-04-12 21:39:46 -0600

    update error handling and add indent

--- rust-json-parser/python/rust_json_parser/__main__.py
+++ rust-json-parser/python/rust_json_parser/__main__.py
@@ -1,27 +1,32 @@
-from os import path
 import sys
+import os
 
 from rust_json_parser._rust_json_parser import parse_json, parse_json_file, dumps
 
-INDENT = 4
-
 
 def main():
-    if sys.argv[1]:
-        target = sys.argv[1]
-        data = None
-        if path.exists(target):
-            try:
-                data = parse_json_file(target)
-                print(dumps(data, INDENT))
-            except Exception as err:
-                print(err)
-        else:
-            try:
-                data = parse_json(target)
-                print(dumps(data, INDENT))
-            except Exception as err:
-                print(err)
+
+    match len(sys.argv):
+        case 2:
+            json = sys.argv[1]
+            indent = None
+
+        case 3:
+            json = sys.argv[1]
+            indent = int(sys.argv[2])
+
+    if os.path.exists(json):
+        try:
+            data = parse_json_file(json)
+            print(dumps(data, indent))
+        except IOError as err:
+            print(f"IO Error: {err}")
+    else:
+        try:
+            data = parse_json(json)
+            print(dumps(data, indent))
+        except ValueError as err:
+            print(f"Invalid JSON: {err}")
 
 
 main()
